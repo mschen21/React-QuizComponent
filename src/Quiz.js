899d1bb1f979e31766d0ecaed839edb271572adb
Steven Chen <steven.m.chen@accenture.com>
2018-06-12 15:26:11 -0500

    finish module 7

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,6 +11,12 @@ class Quiz extends Component {
         this.state = {quiz_position: 1}
     }
 
+    handleResetClick () {
+        this.setState(prevState => ({
+            quiz_position: 1
+        }))
+    }
+
     showNextQuestion () {
         this.setState(prevState => ({
             quiz_position: prevState.quiz_position + 1
@@ -22,7 +28,7 @@ class Quiz extends Component {
         return (
             <div>
                 {isQuizEnd ?
-                    <QuizEnd /> :
+                    <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} /> :
                     <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
                         showNextQuestionHandler={this.showNextQuestion.bind(this)}/>
                 }
